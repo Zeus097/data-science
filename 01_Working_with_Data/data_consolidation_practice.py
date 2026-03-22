211f9fe971769b2d7f5008b237c976c87496784d
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2026-03-22 18:41:57 +0200

    add 'green_tripdata.py'.

--- 01_Working_with_Data/data_consolidation_practice.py
+++ 01_Working_with_Data/data_consolidation_practice.py
@@ -1,15 +1,6 @@
 import pandas as pd
 
 
-
-
-green_tripdata = pd.read_excel("01_Working_with_Data/green_tripdata_2015-09.xls")
-# print(green_tripdata.shape)
-
-
-# ----------------------------------------------------------------
-
-
 library_url = "https://openlibrary.org/api/books?bibkeys=ISBN:9780345354907,ISBN:0881847690,LCCN:2005041555,ISBN:0060957905&format=json"
 library_data = pd.read_json(library_url)
 library_dimensions = library_data.shape
---
+++ 01_Working_with_Data/green_tripdata.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+green_tripdata = pd.read_excel("01_Working_with_Data/green_tripdata_2015-09.xls")
+# print(green_tripdata.shape)
+
+
