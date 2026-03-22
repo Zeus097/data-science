9b865479298af0032b24c45baa10d8f7c2f208ab
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2026-03-22 18:44:28 +0200

    add 'pew_data.py'.

--- 01_Working_with_Data/data_consolidation_practice.py
+++ 01_Working_with_Data/data_consolidation_practice.py
@@ -1,26 +1,6 @@
 import pandas as pd
 
 
-pew_data = pd.read_csv("01_Working_with_Data/pew.csv")
-
-# less_than_10k = pew_data.pivot_table(index="religion", values="<$10k")
-
-
-# print(pew_data)
-# print(less_than_10k)
-
-# print(less_than_10k.index_col())
-
-pew_tidy = pew_data.melt(
-    id_vars=["religion"], # Identifier variables (all others are "unpivoted")
-    var_name="income", # Variable
-    value_name="frequency" # Value
-)
-
-# print(pew_tidy)
-
-
-# ----------------------------------------------------------------
 # TASK:
 #     Tidying process
 #         ▪ First, melt all columns (they are values and should not be)
