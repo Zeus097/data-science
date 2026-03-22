05718b2e4d56c206411d4cd2c8be473831ca2232
Vasil Dimitrov <142613528+Zeus097@users.noreply.github.com>
2026-03-22 18:22:08 +0200

    Practice on extracting data in '.py' foile.

--- 01_Working_with_Data/data_consolidation_practice.py
+++ 01_Working_with_Data/data_consolidation_practice.py
@@ -42,10 +42,24 @@ library_dimensions = library_data.shape
 # ----------------------------------------------------------------
 
 pew_data = pd.read_csv("01_Working_with_Data/pew.csv")
-less_than_10k = pew_data.pivot_table(index="religion", values="<$10k")
+
+# less_than_10k = pew_data.pivot_table(index="religion", values="<$10k")
+
+
 # print(pew_data)
 # print(less_than_10k)
 
 # print(less_than_10k.index_col())
 
+pew_tidy = pew_data.melt(
+    id_vars=["religion"], # Identifier variables (all others are "unpivoted")
+    var_name="income", # Variable
+    value_name="frequency" # Value
+)
+
+# print(pew_tidy)
+
+
 # ----------------------------------------------------------------
+
+
